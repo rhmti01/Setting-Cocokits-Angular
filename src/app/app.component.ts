5b83d4a0b9cee7b2e2560299288b5adcf5ce9acc
rhmti01 <rahmati.business@gmail.com>
2025-04-13 21:03:05 +0330

    add cocokits on app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { input } from "@angular/core";
+
+import { CheckboxComponent } from "@cocokits/angular-checkbox";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CheckboxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Setting-Cocokits-Angular';
+  public readonly Icons = Icons;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideCocokits } from '@cocokits/angular-components';
+import { framesXThemeConfig } from '@cocokits/theme-frames-x';
+import { appConfig } from './app/app.config';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideCocokits(framesXThemeConfig),  
+  ]
+})
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
