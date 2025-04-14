12598bf4bcd50a68e3e1490bfd68511c32248e65
rhmti01 <rahmati.business@gmail.com>
2025-04-14 23:59:26 +0330

    change main.ts config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { input } from "@angular/core";
-
 import { CheckboxComponent } from "@cocokits/angular-checkbox";
 
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,CheckboxComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  template: `
+    <cck-checkbox>I accept the <a href="/">Privacy Policy</a></cck-checkbox>
+  `,
 })
 export class AppComponent {
   title = 'Setting-Cocokits-Angular';
-  public readonly Icons = Icons;
 }
